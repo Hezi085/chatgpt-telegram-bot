39dcf5af3967b71c9592502cd12b8e18deb0a7d5
Həzi <heziheziyev@hotmail.com>
2024-10-01 14:45:34 +0400

    Update telegram_bot.py

--- bot/telegram_bot.py
+++ bot/telegram_bot.py
@@ -40,7 +40,7 @@ class ChatGPTTelegramBot:
         bot_language = self.config['bot_language']
         self.commands = [
             BotCommand(command='help', description=localized_text('help_description', bot_language)+'adam'),
-            BotCommand(command='reset', description=localized_text('reset_description', bot_language))+'salam',
+            BotCommand(command='reset', description=localized_text('reset_description', bot_language)),
             BotCommand(command='stats', description=localized_text('stats_description', bot_language)),
             BotCommand(command='resend', description=localized_text('resend_description', bot_language))
         ]
